c6aeee2e9535a810772284307e9349c54b4253d4
d3monw1ngs <141410698+d3monw1ngs@users.noreply.github.com>
2024-04-07 02:51:20 +0000

    Deploying to gh-pages from @ d3monw1ngs/trial-filmoteka-01@499a4cdc7ff3d34b34c4713eadea7e8b75fb5d15 🚀

--- index.13ffd506.js
+++
@@ -1,2 +0,0 @@
-fetch("https://api.themoviedb.org/3/authentication",{method:"GET",headers:{accept:"application/json",Authorization:"Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWU4MjRhM2Q5MjJmNjhiYTIxMWZjZjZkYmRjYjZmNSIsInN1YiI6IjY2MGUwMGE3YzhhNWFjMDE2Mzc5ODIzOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.dVhYcbmcQhxg-GRwDOu4UQPpqCnjC9js7XxT0ubEx7s"}}).then((function(n){return n.json()})).then((function(n){return console.log(n)})).catch((function(n){return console.error(n)}));
-//# sourceMappingURL=index.13ffd506.js.map
